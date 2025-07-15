3d487f585b375a9643b5f06c9e73df8943d17631
彥東 <wuc656@gmail.com>
2025-07-15 11:29:36 +0800

    測試2

--- module/service.sh
+++ module/service.sh
@@ -4,17 +4,17 @@ MODPATH="${0%/*}"
 # Conditional sensitive properties
 
 # Magisk Recovery Mode
-resetprop_if_match ro.boot.mode recovery unknown
-resetprop_if_match ro.bootmode recovery unknown
-resetprop_if_match vendor.boot.mode recovery unknown
+#resetprop_if_match ro.boot.mode recovery unknown
+#resetprop_if_match ro.bootmode recovery unknown
+#resetprop_if_match vendor.boot.mode recovery unknown
 
 # SELinux
-resetprop_if_diff ro.boot.selinux enforcing
+#resetprop_if_diff ro.boot.selinux enforcing
 # use toybox to protect stat access time reading
-if [ "$(toybox cat /sys/fs/selinux/enforce)" = "0" ]; then
-    chmod 640 /sys/fs/selinux/enforce
-    chmod 440 /sys/fs/selinux/policy
-fi
+#if [ "$(toybox cat /sys/fs/selinux/enforce)" = "0" ]; then
+#    chmod 640 /sys/fs/selinux/enforce
+#    chmod 440 /sys/fs/selinux/policy
+#fi
 
 # Conditional late sensitive properties
 
@@ -24,18 +24,18 @@ done
 
 # SafetyNet/Play Integrity + OEM
 # avoid bootloop on some Xiaomi devices
-resetprop_if_diff ro.secureboot.lockstate locked
+#resetprop_if_diff ro.secureboot.lockstate locked
 # avoid breaking Realme fingerprint scanners
-resetprop_if_diff ro.boot.flash.locked 1
+#resetprop_if_diff ro.boot.flash.locked 1
 #resetprop_if_diff ro.boot.realme.lockstate 1
 # avoid breaking Oppo fingerprint scanners
-resetprop_if_diff ro.boot.vbmeta.device_state locked
+#resetprop_if_diff ro.boot.vbmeta.device_state locked
 # avoid breaking OnePlus display modes/fingerprint scanners
-resetprop_if_diff vendor.boot.verifiedbootstate green
+#resetprop_if_diff vendor.boot.verifiedbootstate green
 # avoid breaking OnePlus/Oppo fingerprint scanners on OOS/ColorOS 12+
-resetprop_if_diff ro.boot.verifiedbootstate green
-resetprop_if_diff ro.boot.veritymode enforcing
-resetprop_if_diff vendor.boot.vbmeta.device_state locked
+#resetprop_if_diff ro.boot.verifiedbootstate green
+#resetprop_if_diff ro.boot.veritymode enforcing
+#resetprop_if_diff vendor.boot.vbmeta.device_state locked
 
 # Other
-resetprop_if_diff sys.oem_unlock_allowed 0
+#resetprop_if_diff sys.oem_unlock_allowed 0
